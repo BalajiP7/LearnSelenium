722d8dbbab2ac16fa35eec7a6af9b4deacf1f943
BalajiP7 <balaji.s@probeseven.com>
2019-08-06 02:55:42 -0930

    Changed the Class Name Main to Sample

--- src/com/company/Sample.java
+++ src/com/company/Sample.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Main {
+public class Sample {
 
     // Initiate webdriver object
     static WebDriver driver;
